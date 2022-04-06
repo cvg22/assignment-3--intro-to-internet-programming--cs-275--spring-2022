346574f79b38d8d77e47545ebfac0de723f019c7
carmen <villalobo@hartford.edu>
2022-04-06 14:35:09 -0400

    Addition of compressHTML

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 const { src, dest, series, watch } = require(`gulp`),
 
-    htmlValidator = require(`gulp-html`);
+    htmlValidator = require(`gulp-html`),
+    htmlCompressor = require(`gulp-htmlmin`);
+
 
     let validateHTML = () => {
         return src(`dev/html/*.html`).pipe(htmlValidator())
         .pipe(dest(`temp`));
     };
 
+    let compressHTML = () => {
+    return src(`dev/html/*.html`)
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
 
 exports.validateHTML = validateHTML;
+exports.compressHTML = compressHTML;
