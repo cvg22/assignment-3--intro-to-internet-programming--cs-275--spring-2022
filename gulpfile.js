4b81caccb37f7a4d84c91f74a120d6fbf7803aba
carmen <villalobo@hartford.edu>
2022-04-06 14:43:39 -0400

    Addition of lintCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const { src, dest, series, watch } = require(`gulp`),
 
     htmlValidator = require(`gulp-html`),
-    htmlCompressor = require(`gulp-htmlmin`);
-
+    htmlCompressor = require(`gulp-htmlmin`)
+    CSSLinter = require(`gulp-stylelint`);
 
     let validateHTML = () => {
         return src(`dev/html/*.html`).pipe(htmlValidator())
@@ -13,7 +13,20 @@ const { src, dest, series, watch } = require(`gulp`),
     return src(`dev/html/*.html`)
         .pipe(htmlCompressor({collapseWhitespace: true}))
         .pipe(dest(`prod`));
-};
+      };
+
+  let lintCSS = () => {
+      return src(`dev/css/style.css`)
+          .pipe(CSSLinter({
+              failAfterError: false,
+              reporters: [
+                  {formatter: `string`, console: true}
+              ]
+          }))
+          .pipe(dest(`temp/css`));
+  };
+
 
 exports.validateHTML = validateHTML;
 exports.compressHTML = compressHTML;
+exports.lintCSS = lintCSS;
