6a84b6f72dcd756ca9f06da755012454f01cdc03
carmen <villalobo@hartford.edu>
2022-04-06 17:16:30 -0400

    Addition of browserSync and compressCSS

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,10 @@ const { src, dest, series, watch } = require(`gulp`),
     CSSLinter = require(`gulp-stylelint`),
     babel = require(`gulp-babel`),
     jsLinter = require(`gulp-eslint`),
-    jsCompressor = require(`gulp-uglify`);
+    browserSync = require(`browser-sync`),
+    cleanCSS = require('gulp-clean-css'),
+    jsCompressor = require(`gulp-uglify`),
+    reload = browserSync.reload;
 
 
     let validateHTML = () => {
@@ -30,6 +33,13 @@ const { src, dest, series, watch } = require(`gulp`),
           .pipe(dest(`temp/css`));
   };
 
+  let compressCSS = () => {
+  return src('dev/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(dest('prod/css'));
+  };
+
+
   let lintJS = () => {
     return src(`dev/js/*.js`)
         .pipe(jsLinter())
@@ -50,9 +60,45 @@ const { src, dest, series, watch } = require(`gulp`),
         .pipe(dest(`prod/js`));
   };
 
+  let browserChoice = `default`;
+
+  let serve = () => {
+    browserSync({
+        notify: true,
+        browser: browserChoice,
+        reloadDelay: 5000,
+        server: {
+            baseDir: `temp`
+        }
+    });
+
+    watch(`dev/js/*.js`, series(lintJS, transpileJSForDev))
+        .on(`change`, reload);
+
+    watch(`dev/css/*.css`, lintCSS)
+        .on(`change`, reload);
+
+    watch(`dev/html/*.html`, validateHTML)
+        .on(`change`, reload);
+};
+
+
 exports.validateHTML = validateHTML;
 exports.compressHTML = compressHTML;
+exports.compressCSS = compressCSS;
 exports.lintCSS = lintCSS;
 exports.lintJS = lintJS;
 exports.transpileJSForDev = transpileJSForDev;
 exports.transpileJSForProd = transpileJSForProd;
+exports.serve = series(
+    validateHTML,
+    lintCSS,
+    lintJS,
+    transpileJSForDev,
+    serve
+);
+
+exports.prod = series(
+          compressHTML,
+		      compressCSS,
+		      transpileJSForProd);
